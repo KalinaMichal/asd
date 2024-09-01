2558b7d72fdfc5ef4e6aab79c39fa16f20b421ff
DESKTOP-AIDSC4O\micha <michal230915@gmail.com>
2024-09-01 21:01:54 +0200

    na pwr

---
+++ pwr/serverSsh.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+const https = require('https');
+const fs = require('fs');
+
+const hostName = 'converetti.com';
+
+const httpsOptions = {
+	cert: fs.readFileSync('./ssl/converetti_com.crt'),
+	ca: fs.readFileSync('./ssl/converetti_com.ca-bundle'),
+	key: fs.readFileSync('./ssl/converetti_com.key')
+};
+
+var app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+const server = https.createServer(httpsOptions, app);
+
+app.use((req, res, next) => {
+   if(req.protocol === 'http') {
+     res.redirect(301, `https://localhost:443`);
+   }
+   next();
+});
+
+app.use((req, res, next) => {
+  if (req.protocol === 'https' && req.hostname === 'www.converetti.com') {
+    return res.redirect(301, `https://converetti.com:443`);
+  }
+  next();
+});
+
+server.listen(443, () => console.log(`App running on: https://localhost:443`));
